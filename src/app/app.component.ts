9d93e00ad4da320bd992351353af71e467412f9c
CHASSANDE BARRIOZ Sebastien <sebastien.chassande@soprasteria.com>
2020-04-07 21:07:54 +0200

    menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
+import { UserService } from 'src/app/service/UserService';
+import { ConnectedUserService } from 'src/app/service/ConnectedUserService';
 import { Component, OnInit } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, MenuController, NavController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -10,45 +12,15 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent implements OnInit {
-  public selectedIndex = 0;
-  public appPages = [
-    {
-      title: 'Inbox',
-      url: '/folder/Inbox',
-      icon: 'mail'
-    },
-    {
-      title: 'Outbox',
-      url: '/folder/Outbox',
-      icon: 'paper-plane'
-    },
-    {
-      title: 'Favorites',
-      url: '/folder/Favorites',
-      icon: 'heart'
-    },
-    {
-      title: 'Archived',
-      url: '/folder/Archived',
-      icon: 'archive'
-    },
-    {
-      title: 'Trash',
-      url: '/folder/Trash',
-      icon: 'trash'
-    },
-    {
-      title: 'Spam',
-      url: '/folder/Spam',
-      icon: 'warning'
-    }
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
 
   constructor(
+    private navController: NavController,
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private userService: UserService,
+    public connectedUserService: ConnectedUserService,
+    private menu: MenuController
   ) {
     this.initializeApp();
   }
@@ -61,9 +33,19 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    const path = window.location.pathname.split('folder/')[1];
-    if (path !== undefined) {
-      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
-    }
+  }
+  public route(url: string = '/home') {
+    console.log('route(', url, ')');
+    this.navController.navigateRoot(url);
+    this.menu.close();
+  }
+
+  public reloadPage() {
+    window.location.reload(true);
+  }
+
+  public logout() {
+    this.userService.logout();
+    this.route('/user/login');
   }
 }
