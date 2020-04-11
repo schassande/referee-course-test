dd752950281f408712c16a4ba92b16174d3574af
CHASSANDE BARRIOZ Sebastien <sebastien.chassande@soprasteria.com>
2020-04-11 14:19:35 +0200

    teacher feature

--- src/app/teacher/teacher-list/teacher-list.component.ts
+++ src/app/teacher/teacher-list/teacher-list.component.ts
@@ -1,5 +1,6 @@
-import { User } from 'src/app/model/model';
-import { AlertController, NavController } from '@ionic/angular';
+import { UserSelectorComponent } from './../../main/widget/user-selector-component';
+import { User, SharedWith } from 'src/app/model/model';
+import { AlertController, NavController, ModalController } from '@ionic/angular';
 import { ConnectedUserService } from 'src/app/service/ConnectedUserService';
 import { UserService } from 'src/app/service/UserService';
 import { Component, OnInit } from '@angular/core';
@@ -16,6 +17,7 @@ export class TeacherListComponent implements OnInit {
   constructor(
     public alertCtrl: AlertController,
     private connectedUserService: ConnectedUserService,
+    private modalController: ModalController,
     private navController: NavController,
     private userService: UserService) { }
 
@@ -26,16 +28,30 @@ export class TeacherListComponent implements OnInit {
   loadTeachers() {
     const region = this.connectedUserService.getCurrentUser().dataRegion;
     this.userService.findTeachers(region).subscribe((ruser) => {
-      console.log('findTeachers(): ', ruser.data);
       this.teachers = ruser.data;
     });
   }
 
-  addTeacher() {
+  async addTeacher() {
+    const modal = await this.modalController.create({ component: UserSelectorComponent});
+    modal.onDidDismiss().then( (data) => {
+      const sharedWith: SharedWith = data.data as SharedWith;
+      if (sharedWith) {
+        sharedWith.users.forEach((user) => {
+          if (user.role === 'LEARNER') {
+            user.role = 'TEACHER';
+            this.userService.save(user).subscribe(() => this.loadTeachers());
+          }
+        });
+      }
+    });
+    modal.present();
   }
+
   deleteTeacher(user: User) {
       if (user.role === 'TEACHER') {
         user.role = 'LEARNER';
+        this.userService.save(user).subscribe(() => this.loadTeachers());
       }
   }
 }
