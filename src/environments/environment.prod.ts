8823124a83db779dd4a13e5ad4191c8de4edecd0
CHASSANDE BARRIOZ Sebastien <sebastien.chassande@soprasteria.com>
2020-04-14 22:54:37 +0200

    'version'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    apiKey: 'XXXXXXXXXXXXXXXXXXXXX',
     authDomain: 'referee-course-test.firebaseapp.com',
     databaseURL: 'https://referee-course-test.firebaseio.com',
     projectId: 'referee-course-test',
     storageBucket: 'referee-course-test.appspot.com',
     messagingSenderId: '18715023816'
   },
-  version: '1.0.1-2020-04-14T20:46:34.238Z'
+  version: '1.0.2-2020-04-14T20:49:16.511Z'
 };
