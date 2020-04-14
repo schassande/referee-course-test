d98d2558d986783b1892e9373741f336a0d05f52
CHASSANDE BARRIOZ Sebastien <sebastien.chassande@soprasteria.com>
2020-04-14 22:48:14 +0200

    remove api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: 'AIzaSyD8ibJnxxxg0fD1wfddSVXfbiXrhd3V7DY',
+    apiKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXX',
     authDomain: 'referee-course-test.firebaseapp.com',
     databaseURL: 'https://referee-course-test.firebaseio.com',
     projectId: 'referee-course-test',
     storageBucket: 'referee-course-test.appspot.com',
     messagingSenderId: '18715023816'
   },
-  version: '1.0.1'
+  version: '1.0.1-2020-04-14T20:46:34.238Z'
 };
