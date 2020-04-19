3cb5e9a18265c22c683fed77905e0659b0cdb32e
CHASSANDE BARRIOZ Sebastien <sebastien.chassande@soprasteria.com>
2020-04-19 20:22:14 +0200

    'version'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,5 +8,5 @@ export const environment = {
     storageBucket: 'referee-course-test.appspot.com',
     messagingSenderId: '18715023816'
   },
-  version: '1.0.5-2020-04-19T18:04:14.206Z'
+  version: '1.0.6-2020-04-19T18:16:18.442Z'
 };
