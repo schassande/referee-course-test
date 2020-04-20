4b38d93661e441af05eeb5ed008882d44dbca437
CHASSANDE BARRIOZ Sebastien <sebastien.chassande@soprasteria.com>
2020-04-20 09:05:43 +0200

    deletion of unused learner account

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,10 @@
-import { sendEmailConfirmation as _sendEmailConfirmation } from './user';
+import { 
+    sendEmailConfirmation as _sendEmailConfirmation, 
+    accountCleanup as _accountCleanup 
+} from './user';
 
+////////////////////////////////////////////////////////////
+////////// Export the function about the user //////////////
+////////////////////////////////////////////////////////////
 export const sendEmailConfirmation = _sendEmailConfirmation;
+export const accountCleanup = _accountCleanup;
--- functions/src/user.ts
+++ functions/src/user.ts
@@ -1,5 +1,11 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 import * as nodemailer from 'nodemailer';
+import * as promisePool from 'es6-promise-pool';
+const PromisePool = promisePool.default;
+
+admin.initializeApp();
+const firestore = admin.firestore();
 
 const gmailEmail = functions.config().gmail.email;
 const gmailPassword = functions.config().gmail.password;
@@ -15,12 +21,15 @@ const mailTransport = nodemailer.createTransport({
 // // https://firebase.google.com/docs/functions/typescript
 //
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Configure the email transport using the default SMTP transport and a GMail account.
-// For other types of transports such as Sendgrid see https://nodemailer.com/transports/
-// TODO: Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
-
-// Sends an email confirmation when a user changes his mailing list subscription.
+/**
+ * Function sending an email to confirm a subscription.
+ * Configure the email transport using the default SMTP transport and a GMail account.
+ * Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
+ */
 export const sendEmailConfirmation = functions.firestore.document('User/{uid}').onCreate(async (snap, context) => {
   // get user detail
   const user: any = snap.data();
@@ -51,3 +60,92 @@ CoachReferee Examinator`
   }
   return null;
 });
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/** Number of day of account inactivity before deletion */
+const NB_DAY_THRESHOLD = 3; // 365;
+/** Maximum concurrent account deletions. */
+const MAX_CONCURRENT = 3;
+
+/**
+ * function deleting unused accounts.
+ */
+export const accountCleanup = functions.pubsub.schedule('every day 00:00').onRun(async context => {
+  // Fetch all user details.
+  const inactiveUsers: UserToDelete[] = await getInactiveUsers();
+  // Use a pool so that we delete maximum `MAX_CONCURRENT` users in parallel.
+  const pool = new PromisePool(() => deleteInactiveUser(inactiveUsers), MAX_CONCURRENT);
+  await pool.start();
+  console.log('User cleanup finished');
+  // TODO send an email to admin with the list of deleted users.
+});
+
+export interface UserToDelete {
+  firebaseId: string;
+  appId: string;
+  toDelete: boolean;
+}
+
+/**
+ * Deletes one inactive user from the list.
+ */
+function deleteInactiveUser(inactiveUsers: UserToDelete[]): Promise<any>|void {
+  if (inactiveUsers.length === 0) {
+    return;
+  }
+  const userToDelete: UserToDelete = inactiveUsers.pop() as UserToDelete;
+  
+  // Delete the user.
+  return new Promise((resolve) => {
+    console.log('DUMMY: Deleting user account' + userToDelete.firebaseId + '/' + userToDelete.appId + ' because of inactivity');
+    resolve();
+  });
+  /*
+  return admin.auth().deleteUser(userToDelete.firebaseId).then(() => {
+    //TODO delete app user account
+    // firestore.collection('User').doc(userToDelete.appId).delete()
+    console.log('Deleted user account', userToDelete.uid, 'because of inactivity');
+  }).catch((error) => {
+    console.error('Deletion of inactive user account', userToDelete.uid, 'failed:', error);
+  });
+  */
+}
+
+/**
+ * Returns the list of all inactive users.
+ */
+async function getInactiveUsers(users:any = [], nextPageToken: any = null): Promise<UserToDelete[]> {
+  const result = await admin.auth().listUsers(1000, nextPageToken);
+  // Find users to delete 
+  let usersToDelete: UserToDelete[] = await Promise.all(result.users.map(async (user) => {
+    const u2d: UserToDelete = { firebaseId: user.uid, appId: '',  toDelete: false }
+  
+    // not signed in in the last 30 days.
+    if (Date.parse(user.metadata.lastSignInTime) >= (Date.now() - NB_DAY_THRESHOLD * 24 * 60 * 60 * 1000)) {
+      return Promise.resolve(u2d);
+    }
+    
+    // check it is not teacher or administrator of the application
+    const appUsers = await firestore.collection('User').where('accountId', '==', user.uid).get();
+    if (appUsers.size === 1) {
+      const appUser: any = appUsers.docs[0].data;
+      if (appUser.role !== 'LEARNER') {
+        return Promise.resolve(u2d);
+      }
+      u2d.appId = appUser.id;
+    } // else the user does not exist => delete it
+
+    u2d.toDelete = true;
+    return Promise.resolve(u2d); // means delete
+  }));
+  usersToDelete = usersToDelete.filter(u => u.toDelete);
+
+  // Concat with list of previously found inactive users if there was more than 1000 users.
+  usersToDelete = users.concat(usersToDelete);
+  
+  // If there are more users to fetch we fetch them.
+  return result.pageToken ? getInactiveUsers(usersToDelete, result.pageToken) : Promise.resolve(usersToDelete);
+}
