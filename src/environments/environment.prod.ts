348af951a8c9a599072beb6e915ef4739d49d2dd
Sebastien Chassande <sebastien.chassande@soprasteria.com>
2020-06-14 17:12:44 +0200

    'version'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,5 +10,5 @@ export const environment = {
     functionRegion: 'us-central1',
     functionOrigin: 'https://referee-course-test.web.app'
   },
-  version: '1.0.9-2020-06-14T14:55:34.740Z'
+  version: '1.0.11-2020-06-14T15:09:31.527Z'
 };
