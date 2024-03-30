176f904e01890e877fca6cee19b7058a57e97707
Sebastien Chassande <chassande@gmail.com>
2024-03-30 12:52:44 +0100

    'version'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,5 +10,5 @@ export const environment = {
     functionRegion: 'us-central1',
     functionOrigin: 'https://referee-course-test.web.app'
   },
-  version: '1.16.0-2024-03-29T20:45:01.704Z'
+  version: '1.16.1-2024-03-30T11:51:13.486Z'
 };
